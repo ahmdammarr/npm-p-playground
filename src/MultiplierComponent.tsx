8d6f4a88f3ef00474b2a2cde135df59b468941e6
ahmedammarr <ahmedmahmoud9597@gmail.com>
2023-02-10 21:11:51 +0400

    hello there

---
+++ src/MultiplierComponent.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MultiplierComponent = () => {
+  return (
+    <View>
+      <Text>MultiplierComponent</Text>
+    </View>
+  )
+}
+
+export default MultiplierComponent
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,2 @@
 export {multiply} from './multiply'
+export {default as MultiplierComponent} from './MultiplierComponent'
